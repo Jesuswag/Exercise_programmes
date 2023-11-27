ad8cfdab99cbdac90f4093d76a97dfd2edb37281
Jesuswag <Jesuswag88@gmail.com>
2023-11-27 17:33:03 +0100

    Exercise 7S is now improved.

--- Unit 6/Unit 6 Exercise 7S.c
+++ Unit 6/Unit 6 Exercise 7S.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <string.h>
-void insertar(int p, char a[], char b[]){
+void insertar(int p, char a[], char b[],int tamanio1){
+    char auxiliar[tamanio1];
+    int contador = 0;
+    for (int i = p;i<=strlen(a);i++){
+        auxiliar[contador] = a[i];
+        contador++;
+    }
+    puts(auxiliar);
     a[p] = '\0';
     strcat(a,b);
+    strcat(a,auxiliar);
 }
 
 int main() {
     int tamanio1, tamanio2,p;
-
-    puts("Tamanio cadena 1?");
-    scanf(" %d",&tamanio1);
-    char a[tamanio1+1];
-    puts("Metela:");
-    scanf(" %s",a);
 
     puts("Tamanio cadena 2?");
     scanf(" %d",&tamanio2);
@@ -20,10 +22,16 @@ int main() {
     puts("Metela:");
     scanf(" %s",b);
 
+    puts("Tamanio cadena 1?");
+    scanf(" %d",&tamanio1);
+    char a[tamanio1+tamanio2+1];
+    puts("Metela:");
+    scanf(" %s",a);
+
     puts("A partir de que posicion del primer string quieres introducir el segundo?");
     scanf(" %d",&p);
 
-    insertar(p,a,b);
+    insertar(p,a,b,tamanio1);
     puts(a);
 
     return 0;
